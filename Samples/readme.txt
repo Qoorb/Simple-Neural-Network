a1312fb8906e079ddc9fa09cd25de4202d68f2a7
olphi <kirichgordeev@gmail.com>
2023-01-15 04:51:48 +0700

    Minor update

---
+++ Samples/readme.txt
@@ -0,0 +1,1 @@
+These samples are extracted from the frames of "Frames_GT_wHolder".
--- mat.py
+++ mat.py
@@ -1,62 +1,72 @@
 import numpy as np
-import random 
-class Neuron():
-    def __init__(self):
-        self.InWeights = np.random.random()
-        self.bias = np.random.rand()
-        self.Value = 0
+# import random 
+# class Neuron():
+#     def __init__(self):
+#         self.InWeights = np.random.random()
+#         self.bias = np.random.rand()
+#         self.Value = 0
     
-    def get_bias(self):
-        return self.bias
+#     def get_bias(self):
+#         return self.bias
     
-    def get_Value(self):
-        return self.Value
+#     def get_Value(self):
+#         return self.Value
 
-    def set_bias(self, b):
-        self.bias = b
+#     def set_bias(self, b):
+#         self.bias = b
 
-    def set_Value(self, v):
-        self.Value = v
+#     def set_Value(self, v):
+#         self.Value = v
     
-    def set_InWeights(self, In):
-        self.InWeights = np.random.random(In)
+#     def set_InWeights(self, In):
+#         self.InWeights = np.random.random(In)
 
-    def Activation(self, x):
-        return sigmoid(np.dot(self.InWeights, x) + self.bias)
+#     def Activation(self, x):
+#         return sigmoid(np.dot(self.InWeights, x) + self.bias)
     
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-(x)))
+# def sigmoid(x):
+#     return 1 / (1 + np.exp(-(x)))
 
 
 
 
-n = Neuron()
-print(n.InWeights)
-n.set_InWeights(3)
-print(n.InWeights)
-# a = np.mat([(1,2,1), (1,2,1), (1,2,3)])
-# b = np.random.rand(3,3)
-# b = np.eye(3, 3)
-# c = np.dot(a, b)
-# f = np.empty((3, 3))
-# g = np.empty(3)
-# i = np.empty(3, Neuron)
-#print(a, "\n", b, "\n", c, "\n", f, sep="\n")
-# print(f, g, sep="\n")
+# n = Neuron()
+# print(n.InWeights)
+# n.set_InWeights(3)
+# print(n.InWeights)
+# # a = np.mat([(1,2,1), (1,2,1), (1,2,3)])
+# # b = np.random.rand(3,3)
+# # b = np.eye(3, 3)
+# # c = np.dot(a, b)
+# # f = np.empty((3, 3))
+# # g = np.empty(3)
+# # i = np.empty(3, Neuron)
+# #print(a, "\n", b, "\n", c, "\n", f, sep="\n")
+# # print(f, g, sep="\n")
 
 
-# h = []
-# ghf = Neuron()
-# # h.append(ghf)
-# # print(h[0].bias)
+# # h = []
+# # ghf = Neuron()
+# # # h.append(ghf)
+# # # print(h[0].bias)
 
 
-# a = np.empty(3, dtype=Neuron)
+# # a = np.empty(3, dtype=Neuron)
 
-# a[0] = ghf
-# b = random.randint(0, 100)
-# print(b)
+# # a[0] = ghf
+# # b = random.randint(0, 100)
+# # print(b)
 
-# a = np.random.rand()
+# # a = np.random.rand()
+# # print(a)
+
+
+# a = np.random.random((3, 3))
 # print(a)
+# a = a.T
+# print(a)
+
+
+
+
