06eea704ceb3eefa4b450da6f2f60175e716d0fa
olphi <kirichgordeev@gmail.com>
2023-02-14 00:00:34 +0700

    new data

--- Attempts/test.py
+++ Attempts/test.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import os
 import csv
+import cv2
 
 def createFileList(myDir, format='.jpg'):
   fileList = []
@@ -13,52 +14,57 @@ def createFileList(myDir, format='.jpg'):
               fileList.append(fullName)
   return fileList
 
-myFileList = createFileList('./Samples/train (only green)')
+# myFileList = createFileList('./Samples/train (only green)')
 
-for file in myFileList:
-    print(file)
-    img_file = Image.open(file)
+# for file in myFileList:
+#     print(file)
+#     img_file = Image.open(file)
 
-    # Параметры исходного изображения
-    width, height = img_file.size
-    format = img_file.format
-    mode = img_file.mode
+#     # Параметры исходного изображения
+#     width, height = img_file.size
+#     format = img_file.format
+#     mode = img_file.mode
 
-    img_grey = img_file.convert('L')
-    img_grey = img_grey.resize((32, 32))
+#     img_grey = img_file.convert('L')
+#     img_grey = img_grey.resize((32, 32))
 
-    # Конвертируем в формат .csv
-    value = np.asarray(img_grey.getdata(), dtype=int).reshape((img_grey.size[1], img_grey.size[0]))
+#     # Конвертируем в формат .csv
+#     value = np.asarray(img_grey.getdata(), dtype=int).reshape((img_grey.size[1], img_grey.size[0]))
     
-    value = value.flatten()
-    value = np.insert(value, 0, 1)
+#     value = value.flatten()
+#     value = np.insert(value, 0, 1)
 
-    with open("train_data.csv", 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(value)
+#     with open("train_data.csv", 'a') as f:
+#         writer = csv.writer(f)
+#         writer.writerow(value)
 
 
 
-myFileList = createFileList('./Samples/test (only green)')
+myFileList = createFileList('./Samples/RC_1')
 
+data = []
+c = 1
 for file in myFileList:
-    print(file)
     img_file = Image.open(file)
 
-    # Параметры исходного изображения
-    width, height = img_file.size
-    format = img_file.format
-    mode = img_file.mode
+    img_hsv = img_file.convert('HSV')
+    img_hsv = img_hsv.resize((32, 32))
 
-    img_grey = img_file.convert('L')
-    img_grey = img_grey.resize((32, 32))
-
+    # print(img_hsv)
     # Конвертируем в формат .csv
-    value = np.asarray(img_grey.getdata(), dtype=int).reshape((img_grey.size[1], img_grey.size[0]))
+    data.append(0)
+    value = np.asarray(img_hsv.getdata(), dtype=int)
+    for i in range(img_hsv.size[0] * img_hsv.size[1]):
+      data.append(value[i][0])
 
-    value = value.flatten()
-    value = np.insert(value, 0, 1)
+    
+    # value = value.flatten()
+    # data = np.insert(data, 0, 1)
+    # 0 = Red
+    # 1 = Green
 
-    with open("test_data.csv", 'a') as f:
+    with open("tututrain.csv", 'a') as f:
         writer = csv.writer(f)
-        writer.writerow(value)
+        writer.writerow(data)
+    
+    data = []
